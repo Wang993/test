f6fefc36de0149e315ad5f2955f8ed58b114035a
unknown <Wang_Yif@126.com>
2019-10-10 09:57:35 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/package.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/package.dir/main.cpp.obj"
+  "libpackage.dll.a"
+  "package.exe"
+  "package.exe.manifest"
+  "package.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
